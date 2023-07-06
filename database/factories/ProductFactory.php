9b9634d807dafad5a20e0734f118519145865d89
Lony <loonydarck@hotmail.com>
2023-07-06 07:16:33 -0300

    Factory Products

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Generator as Faker;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+           'name'       => fake()->unique()->word,
+           'description'=> fake()->sentence(),
+        ];
+    }
+}
